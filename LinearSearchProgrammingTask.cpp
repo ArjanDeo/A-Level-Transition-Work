36d32e10669a6bdebda58a52cade58a0024ff042
Arjan Deo <42519389+ArjanDeo@users.noreply.github.com>
2025-09-03 17:44:43 +0100

    fixed the weird duplication upload

--- LinearSearchProgrammingTask.cpp
+++ LinearSearchProgrammingTask.cpp
@@ -1,50 +1,21 @@
 #include <iostream>
-#include <cstdlib>
+#include <string>
+#include <algorithm>
 int main()
 {
-	int numbers[4]{};
-	int remainingGuesses = 7;
-	for (int i = 0; i < 4; i++)
-	{
-		numbers[i] = rand() % 10;
-	}
-	std::string output = "****";
-	while (true) {
-		if (output.find('*') == std::string::npos) {
-			std::cout << "You guessed the number!" << std::endl;
-			break;
-		}
-		if (remainingGuesses == 0) {
-			std::cout << "You lose!";
-			break;
-		}
-		std::string input;
-		std::cout << "Guesses Remaining: " << remainingGuesses << std::endl;
-		int guess[4]{};
-		std::cout << "Enter a four digit number:" << std::endl;
-		std::cin >> input;
-		if (input.length() != 4) {
-			std::cout << "Please enter a four digit number." << std::endl;
-			continue;
-		}
-		char* p;
-		strtol(input.c_str(), &p, 10);
-		if (*p) {
-			std::cout << "Please enter a four digit number." << std::endl;
-			continue;
-		}
-		for (int i = 0; i < 4; i++)
-		{
-			guess[i] = input[i] - '0';
-		}
-		for (int i = 0; i < 4; i++) {
-
-			if (guess[i] == numbers[i]) {
-				output[i] = input[i];
-			}
-		}
-		std::cout << output << std::endl;
-		remainingGuesses--;
-	}
-
+    std::string states[5] = { "Florida", "Georgia", "Delaware", "Alabama", "California" };
+    std::string userState;
+    std::cout << "Enter the state to find:\n";
+    std::cin >> userState;
+std::transform(userState.begin(), userState.end(), userState.begin(), ::tolower);
+for (int i = 0; i < 5; i++)
+{
+std::transform(states[i].begin(), states[i].end(), states[i].begin(), ::tolower);
+if (states[i] == userState)
+{
+std::cout << "Item found at position " << i << std::endl;
+return 0;
+}
+}
+std::cout << "Item not found" << std::endl;
 }
